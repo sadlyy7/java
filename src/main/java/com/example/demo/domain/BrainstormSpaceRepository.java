06e0b54ceacdf03b71f24ce857fb1a9c58469d4e
K-OTSUSU <sadlyy7@naver.com>
2025-12-05 00:02:54 +0900

    Delete src/main directory

--- src/main/java/com/example/demo/domain/BrainstormSpaceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.domain;
-
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BrainstormSpaceRepository extends JpaRepository<BrainstormSpace, Long> {
-    List<BrainstormSpace> findByTeamId(Long teamId);
-}
--- src/main/java/com/example/demo/service/TeamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.domain.Team;
-
-public interface TeamRepository extends JpaRepository<Team, Long> {
-}
